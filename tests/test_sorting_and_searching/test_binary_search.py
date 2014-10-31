4486fba6dd75dab67c25221653f2384455eda9be
ueg1990 <uehtesham90@gmail.com>
2014-10-31 00:16:56 -0400

    Add unit tests for binary search recursive and iterative

--- tests/test_sorting_and_searching/test_binary_search.py
+++ tests/test_sorting_and_searching/test_binary_search.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sorting_and_searching import binary_search_recursive
+from aids.sorting_and_searching.binary_search import binary_search_recursive, binary_search_iterative
 
 class BinarySearchTestCase(unittest.TestCase):
     '''
@@ -13,6 +13,10 @@ class BinarySearchTestCase(unittest.TestCase):
 
     def test_binary_search_recursive(self):
         result = binary_search_recursive(self.example_1, 0, len(self.example_1) - 1, 10)
+        self.assertEqual(result,3)
+
+    def test_binary_search_iterative(self):
+        result = binary_search_iterative(self.example_1, 0, len(self.example_1) - 1, 10)
         self.assertEqual(result,3)
 
 
