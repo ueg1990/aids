f0be6a089d119283dd507288ad91909ed134dcc6
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-11-11 12:12:35 -0500

    Update insertion_sort.py

--- aids/sorting_and_searching/insertion_sort.py
+++ aids/sorting_and_searching/insertion_sort.py
@@ -1,11 +1,20 @@
 '''
 In this module, we implement insertion sort
-Time complexity: O(n ^ 2)
+Time complexity (worst case): O(n ^ 2) (worst case is aray sorted in reverse order)
+Time complexity (best case): O(n) (for nearly sorted and sorted arrays)
+
 '''
 
 
 def insertion_sort(arr):
     '''
     Sort array using insertion sort
+    
     '''
-    pass
+    for index in range(1,len(arr)):
+        current_value = arr[index]
+        position = index
+        while position > 0 and arr[position-1] > current_value:
+            arr[position] = arr[position-1]
+            position = position - 1
+        arr[position] = current_value
