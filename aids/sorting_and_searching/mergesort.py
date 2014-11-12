4d661b0fcb6f4b130370c010d16a2afec2449456
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-11-12 16:34:28 -0500

    Create mergesort.py

---
+++ aids/sorting_and_searching/mergesort.py
@@ -0,0 +1,16 @@
+'''
+In this module, we implement merge sort
+Time complexity: O(n * log n)
+'''
+
+
+def mergesort(arr):
+    '''
+    Sort array using  mergesort
+    
+    '''
+    pass
+    
+
+def _merge(arr):
+    pass
