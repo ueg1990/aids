ad08307a19fcdb3fda33f8ab444e6067e06265ce
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-11-13 18:11:33 -0500

    Create quicksort.py

---
+++ aids/sorting_and_searching/quicksort.py
@@ -0,0 +1,18 @@
+'''
+In this module, we implement quicksort
+Time complexity (average case) : O(n * log n)
+Time complexity (worse case) : O(n ^ 2)
+
+'''
+
+
+def quicksort(arr, left, right):
+    '''
+    Sort array using  quicksort
+    
+    '''
+    pass
+    
+
+def partition(arr, left, right):
+    pass
