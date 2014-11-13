a2250342891aa529b7488fcea2c0bf0928c8f798
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-11-13 18:07:14 -0500

    Create test_quicksort.py

---
+++ tests/test_sorting_and_searching/test_quicksort.py
@@ -0,0 +1,21 @@
+import unittest
+
+from aids.sorting_and_searching.quicksort import quicksort
+
+class QuicksortTestCase(unittest.TestCase):
+    '''
+    Unit tests for quicksort
+    '''
+
+    def setUp(self):
+        self.example_1 = [18,5,3,1,19,6,0,7,4,2,5]
+
+    def test_mergesort(self):
+        quicksort(self.example_1)
+        self.assertEqual(self.example_1,[0,1,2,3,4,5,5,6,7,18,19])
+        
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
