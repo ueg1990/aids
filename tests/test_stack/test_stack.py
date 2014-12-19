7c117c4555fdf30b3d98a453c7e28245dca0c9b9
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-12-19 12:00:37 -0500

    Add unit tests for push, peek, pop and size

--- tests/test_stack/test_stack.py
+++ tests/test_stack/test_stack.py
@@ -9,16 +9,29 @@ class StackTestCase(unittest.TestCase):
     '''
  
     def setUp(self):
-	pass
+		self.test_stack = Stack()
 
     def test_stack_initialization(self):
-	test_stack = Stack()
-	self.assertTrue(isinstance(test_stack, Stack))
+		self.assertTrue(isinstance(self.test_stack, Stack))
 
     def test_stack_is_empty(self):
-        test_stack = Stack()
-	self.assertTrue(test_stack.is_empty())
+        self.assertTrue(self.test_stack.is_empty())
 
+	def test_stack_push(self):
+		self.test_stack.push(1)
+		self.assertEqual(len(self.test_stack), 1)
+		
+	def test_stack_peek(self):
+		self.test_stack.push(1)
+		self.assertEqual(self.test_stack.peek(), 1)
+		
+	def test_stack_pop(self):
+		self.test_stack.push(1)
+		self.assertEqual(self.test_stack.pop(), 1)
+		
+	def test_stack_size(self):
+		self.test_stack.push(1)
+		self.assertEqual(self.test_stack.size(), 1)
 
     def tearDown(self):
         pass
