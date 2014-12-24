f5501cee5b1e7410a5d19522d14f1c9e49ad9d96
ueg1990 <uehtesham90@gmail.com>
2014-12-24 16:08:35 -0500

    Add unit tests for max_subarray

---
+++ tests/test_sorting_and_searching/test_max_subarray.py
@@ -0,0 +1,27 @@
+import unittest
+
+from aids.sorting_and_searching.max_subarray import max_subarray
+
+class MaxSubArrayTestCase(unittest.TestCase):
+    '''
+    Unit tests for max_subarray
+
+    '''
+
+    def setUp(self):
+        pass
+
+    def test_all_positive(self):
+        pass
+
+    def test_all_negative(self):
+        pass
+
+    def test_positive_and_negative(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
