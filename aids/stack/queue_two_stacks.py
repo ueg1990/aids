0f72c4bf32986aae7a59b2380c5a314038c7ed61
ueg1990 <uehtesham90@gmail.com>
2014-12-29 22:21:35 -0500

    Add Queue implementation using two stacks

---
+++ aids/stack/queue_two_stacks.py
@@ -0,0 +1,40 @@
+'''
+Implement Queue data structure using two stacks
+
+'''
+
+from stack import Stack
+
+class QueueUsingTwoStacks(object):
+	def __init__(self):
+		'''
+		Initialize Queue
+
+		'''
+		self.stack1 = Stack()
+		self.stack2 = Stack()
+
+	def __len__(self):
+		'''
+		Return number of items in Queue
+
+		'''
+		return len(self.stack1) + len(self.stack2)
+
+	def enqueue(self,value):
+		'''
+		Enqueue item to queue
+
+		'''
+		self.stack1.push(value)
+
+	def dequeue(self):
+		'''
+		Dequeue item from queue
+
+		'''
+		if not self.stack2.is_empty():
+			return self.stack2.pop()
+		while not self.stack1.is_empty():
+			self.stack2.push(self.stack1.pop())
+		return self.stack2.pop()
