26cd3cd213625310ae4a42f5c0bfce65510cc05e
ueg1990 <uehtesham90@gmail.com>
2015-01-02 23:02:32 -0500

    Add Node class for Linked Lists

---
+++ aids/linked_list/node.py
@@ -0,0 +1,22 @@
+'''
+Implement a Node that will be used to generate linked lists
+
+'''
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self,new_data):
+        self.data = new_data
+
+    def set_next(self,new_next):
+        self.next = new_next
