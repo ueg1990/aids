34f3b2a08ffded22076e514433d3decb4c6cc2a0
ueg1990 <uehtesham90@gmail.com>
2015-01-03 03:07:48 -0500

    Add class for LinkedList

---
+++ aids/linked_list/linked_list.py
@@ -0,0 +1,53 @@
+'''
+In this module, we implement a Linked List
+
+'''
+
+
+from node import Node
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
