2917f396f52eb042f2354f0a7e1d05dd59b819e3
ueg1990 <uehtesham90@gmail.com>
2015-01-07 17:41:34 -0500

    Add iterative and recursive solutions to reverse strings

--- aids/strings/reverse_string.py
+++ aids/strings/reverse_string.py
@@ -4,10 +4,15 @@ Reverse a string
 '''
 
 def reverse_string_iterative(string):
-	pass
+	result = ''
+	for char in range(len(string) - 1, -1 , -1):
+		result += char
+	return result
 
 def reverse_string_recursive(string):
-	pass
+	if string:
+        return reverse_string_recursive(string[1:]) + string[0]
+    return ''
 
 def reverse_string_pythonic(string):
 	return string[::-1]
