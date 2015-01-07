adaaf43ac0922ff5e1702b1e768f89b0b1a23bd6
ueg1990 <uehtesham90@gmail.com>
2015-01-06 22:06:37 -0500

    Reverse a string

---
+++ aids/strings/reverse_string.py
@@ -0,0 +1,13 @@
+'''
+Reverse a string
+
+'''
+
+def reverse_string_iterative(string):
+	pass
+
+def reverse_string_recursive(string):
+	pass
+
+def reverse_string_pythonic(string):
+	return string[::-1]
