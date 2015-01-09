09596280f90b279379073237a545055dc3302a24
Usman Ehtesham Gul <uehtesham90@gmail.com>
2015-01-08 22:31:15 -0500

    Add unit tests for is_palindrome

---
+++ tests/test_strings/test_is_palindrome.py
@@ -0,0 +1,18 @@
+import unittest
+
+from aids.strings.is_palindrome import is_palindrome
+
+class IsPalindromeTestCase(unittest.TestCase):
+    '''
+    Unit tests for  is_palindrome
+
+    '''
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
