f707df0729a842600c029f8cb0dede6533309b61
ueg1990 <uehtesham90@gmail.com>
2015-01-09 22:36:10 -0500

    Add tests for is_palindrome

--- tests/test_strings/test_is_palindrome.py
+++ tests/test_strings/test_is_palindrome.py
@@ -11,6 +11,15 @@ class IsPalindromeTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_is_palindrome_true_empty_string(self):
+    	self.assertTrue(is_palindrome(''))
+
+    def test_is_palindrome_true(self):
+    	self.assertTrue(is_palindrome('madam'))
+
+    def test_is_palindrome_false(self):
+    	self.assertFalse(is_palindrome('xxyyzz'))
+
     def tearDown(self):
         pass
 
